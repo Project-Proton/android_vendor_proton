525d156ea3588dd50cec2bd419a790f03144db47
tmak2002 <tmak@disroot.org>
2021-04-30 12:40:30 +0200

    update

--- prebuilt/Apps/download.sh
+++ prebuilt/Apps/download.sh
@@ -4,15 +4,15 @@
 # Nextcloud (3.15.1)
 rm nextcloud.apk 
 wget -O nextcloud.apk https://f-droid.org/repo/com.nextcloud.client_30150190.apk
-# DAVx⁵ (3.3.9-ose)
+# DAVx⁵ (3.3.10-ose)
 rm davdroid.apk
-wget -O davdroid.apk https://f-droid.org/repo/at.bitfire.davdroid_303090004.apk
+wget -O davdroid.apk https://f-droid.org/repo/at.bitfire.davdroid_303100003.apk
 # Bromite arm64
 rm arm64_ChromePublic.apk
-wget https://github.com/bromite/bromite/releases/download/90.0.4430.59/arm64_ChromePublic.apkk
+wget https://github.com/bromite/bromite/releases/download/90.0.4430.101/arm64_ChromePublic.apk
 # Bromite arm
 rm arm_ChromePublic.apk
-wget https://github.com/bromite/bromite/releases/download/90.0.4430.59/arm_ChromePublic.apk
+wget https://github.com/bromite/bromite/releases/download/90.0.4430.101/arm_ChromePublic.apk
 # Etar (1.0.25)
 rm etar.apk
 wget -O etar.apk https://f-droid.org/repo/ws.xsoh.etar_27.apk
